df466ca9849fe014680a510a42fc0e17713603aa
Franz Huayta <fhuayta@agetic.gob.bo>
2022-04-06 11:04:47 -0400

    Commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# correo
+# Envío de correo
 Envío de correo con nodemailer
+
+
+## Ejemplo de uso de la libreria
+
+Para llamar a la libreria
+
+```js
+
+let adjuntosBase = [
+    {
+        filename: 'manual_titulo.txt',
+        path: 'data:text/plain;base64,aGVsbG8gd29ybGQ='
+    },
+    {
+        filename: 'manual_titulo_ADJ2.txt',
+        path: 'data:text/plain;base64,aGVsbG8gd29ybGQ='
+    }
+];
+let smtpOpciones = {
+    user: '<USUARIO-MAIL>@dominio.com>',
+    pass: '<PASSWORD-MAIL>',
+    host: 'smtp.<HOST-SMTP>.com',
+    port: 587,
+    idMensaje: '152'
+}
+
+let mensaje = {
+    asunto: 'Asunto del mensaje',
+    cuerpo: 'Cuerpo del mensaje text o html',
+    destinatarios: ["correo1@dominio.com", "correo2@dominio.com", "correo13@dominio.com"]
+}
+
+sendmail(mensaje, adjuntosBase, smtpOpciones);
+```
---
+++ index.js
@@ -0,0 +1,56 @@
+let nodemailer = require('nodemailer');
+
+let sendmail = function (mensaje, adjuntosBase, smtp) {
+
+    // VALIDACIONES
+    if (mensaje.destinatarios == undefined || mensaje.destinatarios.length === 0) {
+        let respuesta = {
+            mensaje: "No se encontraron destinatarios del correo",
+            idMensaje: smtp.idMensaje,
+            enviado: false
+        }
+        return respuesta;
+    }
+    //---------------------------INICIO
+    let correosDest = '';
+    for (let i = 0; i < mensaje.destinatarios.length; i++) {
+        correosDest = correosDest + ', ' + mensaje.destinatarios[i];
+    }
+    let message = {
+        from: smtp.user,
+        to: correosDest,
+        subject: mensaje.asunto,
+        // text: 'mensaje solo texto',
+        attachments: adjuntosBase,
+        html: mensaje.cuerpo
+    }
+    let transport = nodemailer.createTransport({
+        host: smtp.host,
+        port: smtp.port,
+        secure: false,
+        auth: {
+            user: smtp.user,
+            pass: smtp.pass
+        },
+        tls: { rejectUnauthorized: false }
+    });
+    transport.sendMail(message, function (err) {
+        if (err) {
+            console.log("Error al envio de mail. ", err.message);
+            let respuesta = {
+                mensaje: "Error al envio de mail. " + err.message,
+                idMensaje: smtp.idMensaje,
+                enviado: false
+            }
+            return respuesta;
+        }
+        console.log("Email enviado.");
+        let respuesta = {
+            mensaje: "Email enviado.",
+            idMensaje: smtp.idMensaje,
+            enviado: true
+        }
+        return respuesta;
+    });
+    //---------------------------
+}
